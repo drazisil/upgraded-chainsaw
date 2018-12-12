62a1b4eb398e5fb52558ad5dbfda11bc43f54c89
Joe Becher <jwbecher@drazisil.com>
2018-12-12 08:45:22 -0500

    add tests to inclrease code coverage

--- src/index.js
+++ src/index.js
@@ -2,6 +2,27 @@ exports.hello = () => {
   return 'Hello, new world'
 }
 
-exports.main = () => {
-  console.log(this.hello())
+exports.trimArgs = args => {
+  return args.slice(2)
+}
+
+exports.usage = () => {
+  return `Error: missing arguments.
+    Usage: upgraded-chainsaw <filename>`
+}
+
+exports.checkArgsLength = async args => {
+  if (args.length === 0) {
+    throw new Error(this.usage())
+  }
+  return args
+}
+
+exports.main = async args => {
+  try {
+    console.dir(await this.checkArgsLength(this.trimArgs(args)))
+    console.log(this.hello())
+  } catch (error) {
+    console.error(error.message)
+  }
 }
